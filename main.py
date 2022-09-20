42d08003c8380c2832bf8031f743d0aed021fa40
KeanoW <winterkeano02@gmail.com>
2022-09-20 16:37:32 +0200

    Adding views to html

--- main.py
+++ main.py
@@ -2,9 +2,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return render_template("employee/base.html")
+    return render_template("employee/Events.html")
 
+@app.route("/requests")
+def requests():
+    return render_template("cleaner/Requests.html")
+
+@app.route("/request")
+def request():
+    return render_template("employee/CreateRequest.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
